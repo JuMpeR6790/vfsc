b743d6f882860e54579551789da8613b650dc53b
JuMpeR6790 <64857511+JuMpeR6790@users.noreply.github.com>
2021-12-29 23:21:03 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,16 @@
 import vfsc
 
-def simple_client():
-    v = vfsc()
-    v.connect_to("192.168.1.95", 17224)
+def send_message():   
     while True:
         message = input("chat: ")
         v.send(message)
 
-simple_client()
+def receive_message():
+    while True:
+        print(v.listen())
+
+v = vfsc()
+v.connect_to("192.168.1.95", 17234)
+receive = threading.Thread(target=receive_message)
+receive.start()
+send_message()
