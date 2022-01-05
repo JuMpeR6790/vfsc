a21b03ad1b09a90a31c78140ace118011ea8b948
JuMpeR6790 <64857511+JuMpeR6790@users.noreply.github.com>
2022-01-05 21:08:12 +0000

    Create setup.py

---
+++ vfsc/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name:"vfsc",
+    url:"https://github.com/JuMpeR6790/vfsc/new/main/vfsc",
+    author:"JuMpeR6790,
+    author_email:"JuMpeR6790@gmail.com",
+    packages:["src"],
+    install_requires=['socket'],
+    version='0.1',
+    license:"MIT",
+    description:"VFSC (very fancy socket communicator) is a very simple (but fancy) python api to create simple chat clients (text only)."
+)
